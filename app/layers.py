7a0dd56a99e5e08b26e82a1eacbcceb8c14c99c3
yoonpaix <babuvadakemu@gmail.com>
2023-07-14 20:52:40 +0100

    Adding files for Face Recognition

---
+++ app/layers.py
@@ -0,0 +1,11 @@
+#Custom L1 Distance layer module
+import tensorflow as tf
+from tensorflow.keras.layers import Layer
+
+class L1Dist(Layer):
+    def __init__(self, **kwargs):
+        super().__init__()
+
+    def call(self, input_embedding, validation_embedding):
+        return tf.math.abs(input_embedding - validation_embedding)
+    
